f5d2458a8768c285ea80c692dec038112498aa68
Matthias Hennemeyer <mhennemeyer@gmail.com>
2020-11-05 12:16:17 +0100

    bugfix

--- Sources/swift-static-content/Domain/InformationSwitch.swift
+++ Sources/swift-static-content/Domain/InformationSwitch.swift
@@ -1,6 +1,11 @@
 import Foundation
 
-struct InformationSwitch {
+public struct InformationSwitch {
+    public init(action: @escaping (Bool) -> Void, isOn: Bool) {
+        self.action = action
+        self.isOn = isOn
+    }
+    
     let action: (Bool) -> Void
     let isOn: Bool
 }
